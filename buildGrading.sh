bd8e4373b71f0ab8efe459fb29fa746756a48417
Mark Wissink <mwissink.b@gmail.com>
2018-09-15 17:10:17 -0400

    first commit

---
+++ buildGrading.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+# Author: Joel Stehouwer, jbs24
+# Modified by binki and mcw33 somewhat
+# Use this to grade cs112 labs super awesome-like!
+# /home/cs/112/current$
+
+# don't mess things up if things get messed up
+set -e
+
+# Ensure we are in a proper directory
+if ! [[ ${PWD} =~ ^/home/cs/[[:digit:]]+/current$ ]]; then
+  echo "you done be in the wrong directory" >&2
+  exit 1
+fi
+
+if ! [[ $1 =~ ^[[:digit:]]+$ ]]; then
+  echo "you must pass the current lab as a parameter like: Usage: ${0} 1" >&2
+  exit 1
+fi
+
+if [[ $1 =~ ^0 ]]; then
+  echo "you should not prefix your lab number with a zero. Try again." >&2
+  exit 1
+fi
+
+home=$(pwd) # This will be cs/112/current
+labNum="${1}" # This is the number of the lab
+ignoreRule="${2}" # -i to run only for specified students, -e to exclude specified students
+gradedir=${HOME}/Documents/gradelab${labNum}
+mkdir -p "${gradedir}"
+catfile=${gradedir}/output.txt # The file that this script will write to
+
+ls
+
+doCommand() {
+  echo "Running ${@}" >> $catfile
+  { "${@}" 2>&1 || true; } >> $catfile
+  echo "Finished ${@}" >> $catfile
+  echo >> $catfile
+}
+
+for dir in ./*; do
+  echo $dir
+
+  isIgnored=
+    for studentDir in "$@"; do
+      [[ "${dir##*/}" == "${studentDir}" ]] && isIgnored=1
+    done
+
+  if [[ "${isIgnored}" && "${ignoreRule}" == "-e" ]]; then
+    echo "skipping ${dir}"
+    echo "skipped ${dir}" >> $catfile
+  elif [[ ! "${isIgnored}" && "${ignoreRule}" == "-i" ]]; then
+    echo "skipping ${dir}"
+  else
+    for labDir in "${dir}"/[lL]{,ab}{,0}"${labNum}"; do
+      if ! [[ -e ${labDir} ]]; then
+        echo "Unable to find lab results in $dir: $(ls "${dir}")" >> $catfile
+      else
+        pushd "${labDir}"
+        echo "${labDir}" >> $catfile
+        ls >> $catfile
+    	  echo >> $catfile
+        # Allow g++ to fail because the student may have failed, just like Kristofer Paul Gunadi Brink
+        sources=()
+        for f in *.cpp *.c++ *.cxx; do
+          [[ -e ${f} ]] && sources+=(${f})
+        done
+        doCommand g++ "${sources[@]}" -Wall -o ~/go
+    	  echo >> $catfile
+        # Allow to fail becaus ethe student may have failed. Period.
+        doCommand timeout 5 ~/go
+        cat *.h >> $catfile
+        cat *.cpp >> $catfile
+        studentdir=${gradedir}/${dir}
+        for extraFile in *.ods *.xls*; do
+          if [[ -e ${extraFile} ]]; then
+            # do not make a student dir if there aren't special files to put there in the first place
+            mkdir -p "${studentdir}"
+            cp -vi "${extraFile}" "${studentdir}"/ >> $catfile
+          fi
+        done
+        popd
+      fi
+    done
+    echo -e "$dir\n\n\n" >> $catfile
+  fi
+done
---
+++ gradeFiles.py
@@ -0,0 +1,96 @@
+'''
+File name: createGradeFiles.py
+Author: Mark Wisink
+Python Version: 3.7
+Purpose: Automatically creates grade/feedback files for students from the csv grade file
+'''
+import argparse
+import csv
+import io
+
+parser = argparse.ArgumentParser(description='Creates feed back files for students based on csv')
+parser.add_argument(
+    'lab',
+    metavar='L',
+    help='the lab number'
+)
+parser.add_argument(
+    'csv_path',
+    metavar='P',
+    help='the path to the csv, expected column format: (student, error, location, points, feedback)'
+)
+parser.add_argument(
+    '--m',
+    dest='success_message',
+    default='Great job!',
+    help='A message to display to successful students'
+)
+parser.add_argument(
+    '--d',
+    dest='delete_files',
+    action='store_true',
+    help='Delete the grade files associated with the lab number'
+)
+
+args = parser.parse_args()
+
+# This function has to be modified to find the correct directory
+def get_gradefile(student):
+    return './' + student + '/lab' + args.lab + '.txt'
+
+if (args.delete_files):
+    exit()
+
+def deduct_points(str_points):
+    if str_points == '':
+        return 0
+    try:
+        return int(str_points)
+    except ValueError:
+        print(ValueError)
+        return 0
+
+print('Opening', args.csv_path)
+students = {}
+# Parse each row of the csv and sort them into a dictionary of students
+with open(args.csv_path, 'r', newline='', encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    for row in csv_reader:
+        student = row['student']
+        if (student in students):
+            students[student].append(row)
+        else:
+            students[student] = [row]
+
+print('\nGrades')
+for student in students:
+    grade = 100
+    grade_file = open(get_gradefile(student), 'w')
+    # Write the errors
+    grade_file.write('Lab ' + args.lab + '\n\nErrors\n')
+    errors = ''
+    for row in students[student]:
+        # Deduct the points from the grade
+        grade -= deduct_points(row['points'])
+        if row['error'] != '':
+            location = row['location'] + ' : ' if row['location'] else ''
+            errors += location + row['error'] + '\n'
+    if errors == '':
+        grade_file.write('none\n')
+    else:
+        grade_file.write(errors)
+
+    # Write the feedback
+    grade_file.write('\nFeedback\n')
+    feedback = ''
+    for row in students[student]:
+        if row['feedback'] != '':
+            feedback += row['feedback'] + '\n'
+    if grade == 100:
+        feedback += args.success_message + '\n'
+    grade_file.write(feedback)
+
+    # Write the final grade
+    grade_file.write('\nGrade\n' + str(grade) + '%\n')
+    grade_file.close()
+    print(student, grade)
